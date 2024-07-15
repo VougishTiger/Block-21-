ed1e01e88c2cf5e5aa635045736f069469d7e90a
Robin <vougishtiger187@gmail.com>
2024-07-15 07:13:59 -0700

    main

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.addEventListener(`DOMContentLoaded`, async()=> {
   // call the api to grab the eventInfo using fetch and then tranlate it into eventPlans using .json
   const response= await fetch(` https://fsa-crud-2aa9294fe819.herokuapp.com/api/2046-ftb-et-web-ft/events`);
-  const responceJson= await response.json();
-  const event= responceJson.data;
+  const responseJson= await response.json();
+  const event= responseJson.data;
   // create a new eventList and put all the information on the eventList using innerHTML
   const eventList= document.createElement(`li`);
   eventList.innerHTML= `
-    <h2>${event}</h2>
+    <h2>${event.name}</h2>
     `
     const ul= document.querySelector(`ul`);
     ul.append(eventList);
